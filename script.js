bb338f1868c440e6150c56621a20b6a211c86a68
yusun-lab <yu.sun@hyperisland.se>
2025-01-04 15:40:31 +0100

    handle Start Game

--- script.js
+++ script.js
@@ -6,15 +6,41 @@ const Gameboard = (() => {
         gameboard.forEach((square, index) => {
             boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
         });
+        document.querySelector("#gameboard").innerHTML = boardHTML;
     }
-    document.querySelector("#gameboard").innerHTML = boardHTML;
+ 
+    return {
+        render,
+    }
+})();
 
-    // return {
-    //     render,
-    // }
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    };
+};
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+    return {
+        start,
+    }
 })();
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
-    // Game. start();
+    Game.start();
 })
